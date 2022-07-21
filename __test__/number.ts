0f1ce609005dfb950ac3a88bca5f916a82b9d08a
grh-gitee <3494242908@qq.com>
2022-07-21 11:18:47 +0800

    test: add cases

--- __test__/number.ts
+++ __test__/number.ts
@@ -1,5 +1,8 @@
 /* eslint-disable*/
-import { IsZero, IsOverZero, IsLessZero } from '../src/index'
+import {
+	IsZero, IsOverZero, IsLessZero,
+	IsNumberEqual, IsNumberNotEqual, IntAddSingle
+} from '../src/index'
 
 
 {
@@ -11,5 +14,11 @@ import { IsZero, IsOverZero, IsLessZero } from '../src/index'
 	const t6: IsLessZero<'1'> = false
 	const t7: IsLessZero<'-1'> = true
 	const t8: IsLessZero<'0'> = true
+
+	const t9: IsNumberEqual<1, 3> = false
+	const t10: IsNumberNotEqual<1, 3> = true
+
+	const t11: IntAddSingle<3, 4> = 7
+	const t13: IntAddSingle<3, 999>  = 1002
 }
 
